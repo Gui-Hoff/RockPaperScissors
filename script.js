cff982fed6b60e582aad240652bdd012731edbd2
Gui Hoff <guihoff13@gmail.com>
2022-04-04 14:31:42 -0300

    add game function

--- script.js
+++ script.js
@@ -11,8 +11,69 @@ function computerPlay (){
         play = 'scissors';
         
     }
-    console.log(play);
     return play;
 }
 
-computerPlay();
+
+let playerSelection = theGame(window.prompt('Rock, Paper or Scissors?: '));
+
+function theGame(player){
+
+    let choice = player.toLowerCase();
+    let computer = computerPlay();
+
+    if(choice === 'paper'){
+        if (computer === 'paper'){
+            
+            console.log('Draw!');
+
+        }else if(computer === 'rock'){
+
+            console.log('Rock ... You WIN!!!');
+     
+        }else {
+
+            console.log('Scissors ...You LOSE!');
+
+        }
+    
+
+    }else if (choice === 'rock'){
+        
+        if (computer === 'paper'){
+
+            console.log('Paper ...You LOSE!');
+
+        }else if(computer==='scissor'){
+
+            console.log('Scissors ... You WIN!!!');
+
+        }else{
+
+            console.log('Draw!');
+
+        }
+
+    //choice === scissors    
+    }else if(choice === 'scissors') {
+
+        if(computer === 'paper'){
+
+            console.log('Paper... You WIN!!!');
+
+        }else if (computer === 'rock'){
+
+            console.log('Rock ...You LOSE!');
+
+        }else{
+
+            console.log('Draw!');
+        }
+
+    }else if (choice!=='paper' && choice !=='rock' && choice !== 'scissors' ){
+        console.log('Invalid option ...please choose again.');
+    }
+}
+
+
+
